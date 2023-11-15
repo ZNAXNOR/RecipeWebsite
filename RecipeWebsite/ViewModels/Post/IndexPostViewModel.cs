bbc0f18a66cb3cb0ed717849864bca1429ca9c66
surendra dalvi <surendrasdalvi@gmail.com>
2023-11-15 09:14:33 +0530

    Created sidebar

--- RecipeWebsite/ViewModels/Post/IndexPostViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipeWebsite.ViewModels.Post
-{
-    public class IndexPostViewModel
-    {
-        public IEnumerable<Models.Post> Post { get; set; }
-        public Enum Enum { get; set; }
-    }
-}
